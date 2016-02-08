516e3638873a04971f8e2dee85eb8b0713c90ec7
Adrián Úbeda <adrianubedaportugues@gmail.com>
2016-02-08 12:14:08 +0100

    Ejercicio 13.5 hecho

--- ejercicio.py
+++ ejercicio.py
@@ -8,6 +8,19 @@ fd = open('/etc/passwd', 'r')
 lineas = fd.readlines()
 fd.close()
 
+Diccionario = {}
+
 for linea in lineas:
     elementos = linea.split(':')
     print elementos[0], elementos[-1][:-1]
+    Diccionario[elementos[0]] = elementos[-1][:-1]
+
+print "Hay", len(lineas), "usuarios"
+
+print "\n"
+
+try:
+	print Diccionario["root"]
+	print Diccionario["imaginario"]
+except KeyError:
+	print "El usuario buscado no existe"
